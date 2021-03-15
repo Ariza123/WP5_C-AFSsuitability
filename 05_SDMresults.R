653b690f2fd2593981d775a99616b27397d9b1f0
Pablo <pablo.gonzalez@uco.es>
2021-03-15 18:19:02 +0100

    añadir máscara y gráfica de idoneidad vs bio4

--- 05_SDMresults.R
+++ 05_SDMresults.R
@@ -20,7 +20,7 @@ library(viridis)
 setwd("C:/Users/Pablo/OneDrive - Universidad de Córdoba/1_proyectos/2019_CocoAgroForecast/WP5 - suitability")
 BD_calib <- read.csv("./BD/BD_calibrate.csv")
 sp <- unique(BD_calib$species)
-species <- sort(sp) # hacer acrónimos
+spnames <- sort(sp) # hacer acrónimos
 # .........................................
 # .........................................
 # Prepare data and files to process maps ####
@@ -99,7 +99,7 @@ dev.off()
 
 
 ###################################
-## Key variables per species
+## Key variables per species ####
 
 names(importance_full) <- c("BIO2","BIO13","pH","BIO4","BIO18","BIO9","BIO8","BIO19","CEC","N","SAND","BIO15","SPECIES")
 
@@ -123,3 +123,91 @@ png(filename = "processing/figures/SDMimportance.png",
     ylab("Variable importance (%)")
 
 dev.off()
+
+######################
+## PLOT IDONEIDAD VS BIO4 ####
+
+# Prepare data and files to process maps ####
+# names of RCP scenarios
+RCP <- c("ssp126","ssp585")
+scenario <- c("current","2021-2040","2041-2060")
+#define extention of study area
+ext <- raster::extent(-17,15,2,13)
+# define projection
+proj <- "+proj=longlat +datum=WGS84"
+
+# Load BIO4
+common_path<-"./BD/Current/"
+files <- list.files(
+  path <- common_path,
+  pattern <- "\\.tif$",
+  recursive = F,          
+  full.names = TRUE          
+)
+bio_4 <- crop(raster(files[15]),ext)
+
+stats_especie_all <- NULL
+
+for (i in spnames) {
+  for (s in seq_along(scenario)){
+    for (j in RCP) {
+      
+      if (scenario[s]=="current") {
+      cocoa <- raster("processing/enm/Theobroma cacao/ensembles/suitability/Theobroma cacao__bio_current.grd")
+      r <- raster(paste("processing/species_sets/", i, "/", i, "_presence.tif",sep="" )) 
+      stats_especie <- data.frame(array(dim = c(1, 4)))
+      names(stats_especie) <- c("specie","scenario","cocoa","bio4")
+      stats_especie[1,1] <- i
+      stats_especie[1,2] <- paste(scenario[s],sep="_")
+      stats_especie[1,3] <- mean(values(mask(cocoa,r,maskvalue=0)),na.rm=T)
+      stats_especie[1,4] <-  mean(values(mask(bio_4,r,maskvalue=0)),na.rm=T)
+      
+      stats_especie_all <- rbind(stats_especie_all,stats_especie)
+      
+      }else{
+      cocoa <- raster(paste("processing/species_sets/Theobroma cacao/Theobroma cacao_", j ,"_",scenario[s], "_presence_agreement.tif",sep=""))
+      r <- raster(paste("processing/species_sets/", i, "/", i, "_", j ,"_",scenario[s],"_presence.tif",sep="" ))
+      
+      stats_especie <- data.frame(array(dim = c(1, 4)))
+      names(stats_especie) <- c("specie","scenario","cocoa","bio4")
+      stats_especie[1,1] <- i
+      stats_especie[1,2] <- paste(scenario[s],j,sep="_")
+      stats_especie[1,3] <- mean(values(mask(cocoa,r,maskvalue=0)),na.rm=T)
+      stats_especie[1,4] <-  mean(values(mask(bio_4,r,maskvalue=0)),na.rm=T)
+      
+      stats_especie_all <- rbind(stats_especie_all,stats_especie)
+      
+      }
+      
+    }
+  }
+}
+
+stats_especie_all[stats_especie_all$scenario=="current","cocoa"] <- stats_especie_all[stats_especie_all$scenario=="current","cocoa"]/1000
+stats_especie_all <- unique.data.frame(stats_especie_all)
+# export dataframe
+write_csv(stats_especie_all, paste0("processing/figures/suitability_vs_bio4.csv"))
+
+
+
+
+library(ggrepel)
+png(filename = "processing/figures/suitability_vs_bio4.png",
+    width = 600, height = 600, units = "px", pointsize = 18,
+    bg = "white")
+
+ggplot(subset(stats_especie_all,scenario=="current"),aes(x=cocoa, y=bio4/100,label=specie))+
+  geom_point() + geom_text_repel() +
+  scale_fill_viridis(discrete=TRUE) +
+  scale_color_viridis(discrete=TRUE) +
+  theme_bw(base_size = 16) + theme(legend.position="none") +
+  xlab("Cocoa suitability") +
+  ylab("Temperature seasonality")
+
+#  annotation_custom(,xmin=0.6, xmax=1, ymin=1.4, ymax=1.6) #para añadir algo dentro
+  
+  
+
+dev.off()
+
+plot(bio_4/100)
--- 06_wining_losing_sp.R
+++ 06_wining_losing_sp.R
@@ -1,7 +1,8 @@
 ####################################################
 # #### CALCULATE THE CHANGES IN SUITABILITY OF FOCAL SPECIES 
 # IN FUTURE SUITABLE AREAS FOR COFFEE AND COCOA
-# This script generates the dataframe for Fig. 4
+
+# This script generates the figure of change of suitability in area per species
 
 #..............................................
 #..............................................
@@ -12,6 +13,7 @@ library("magrittr")
 library("raster")
 library("gridExtra")
 library("scales")
+library(rgdal)
 
 #..............................................
 #..............................................
@@ -34,9 +36,21 @@ proj <- "+proj=longlat +datum=WGS84"
 
 #..............................................
 #..............................................
-# Read changes of focal species ####
+# Load mask
+mascara <- "processing/capa_mask/Cropland.shp"
+mascara %<>%
+  readOGR(.) %>%
+  raster::crop(. , ext)
+
+
+# plot(mascara)
+  
+
+
+# AREA SUITABLE WITHOUT MASK ####
+# Read changes of focal species ###
 # this loop calculate the frequencies of suitability of each 
-# focal species using the mask generated above.
+# focal species NOT using the mask generated above.
 changes <- NULL
 
 for (i in spnames) {
@@ -91,16 +105,17 @@ for (i in spnames) {
    }
   }
   
-  # changes %<>% 
-  #   inner_join(., sp, by="acronym") %>% 
-  #   filter(. , !grepl("current", system)) 
-  
+
   # export dataframe
   write_csv(changes, paste0("processing/figures/wining_losing_sp.csv"))
   
   #..............................................
   #..............................................
-  # Calculate changes ####
+ 
+  
+  
+   # Calculate changes ####
+  changes <- read.csv("processing/figures/")
   changes <- changes[,-c(7:11)]
   names(changes) <- c("acronym","System","never","remain","no_longer","new")
   
@@ -143,19 +158,19 @@ for (i in spnames) {
       lab <- ""
       
       p <- ggplot(data=df) +
-        geom_point(aes(x=cover,y=id), pch  = 21, size=2, fill="grey",colour="black") +
         geom_segment(data=subset(df, df$change <= 0), 
                      mapping = aes(x=cover, xend=future_cover,
                                    y=id, yend=id),
-                     arrow=arrow(length = unit(0.1, "cm")),
-                     size=1, 
+                     # arrow=arrow(length = unit(0.5, "cm")),
+                     size=5, 
                      color= "#ca0020") +
         geom_segment(data=subset(df, df$change > 0), 
                      mapping = aes(x=cover, xend=future_cover,
                                    y=id, yend=id),
-                     arrow=arrow(length = unit(0.1, "cm")),
-                     size=1, 
-                     color= "#0571b0") +
+                     # arrow=arrow(length = unit(0.5, "cm")),
+                     size=5, 
+                     color= "green4") +
+        geom_segment(aes(x=cover-0.5,xend=cover+0.5,y=id,yend=id), size=5, colour="black")+
         scale_y_continuous(breaks = as.integer(df$id), labels = df$acronym) +
         scale_x_continuous(breaks = seq(0,100, by=25), limits = c(0,100)) +
         labs(x = paste0("Suitable area (%)"),
@@ -202,4 +217,172 @@ for (i in spnames) {
          height = 15,
          units = "cm")
 
+  #################################################################
+  # AREA SUITABLE WITH MASK ####
+  # Read changes of focal species ###
+  # this loop calculate the frequencies of suitability of each 
+  # focal species USING the mask generated above.
+  changes <- NULL
+  
+  for (i in spnames) {
+    for (s in seq_along(scenario)){
+      for (j in RCP) {
+        
+        if (j=="current") {
+          r <- raster(paste("processing/species_sets/", i, "/", i, "_presence.tif",sep="" )) 
+          r <- raster::mask(r , mascara, inverse = FALSE) 
+        }else{
+          r <- raster(paste("processing/species_sets/", i, "/", i, "_", j ,"_",scenario[s],"_change.tif",sep="" ))
+          r <- raster::mask(r , mascara, inverse = FALSE)
+        }
+        
+        land <- r
+        frequencies <- data.frame(array(dim = c(1, 11)))
+        names(frequencies) <- c("acronym","system","never_suitable(0)", "remains_suitable(1)", 
+                                "no_longer_suitable(-1)", "new_habitat(2)","area_km2","remains_suitable_(1)_perc",
+                                "no_longer_suitable(-1)_perc", "new_habitat_(2)_per","net_change_perc")
+        freq1 <- as.data.frame(raster::freq(land, useNA="no"))
+        freq1 <- freq1[is.na(freq1[, 1]) == F, ]
+        
+        frequencies[1,1] <- i
+        frequencies[1,2] <- paste(scenario[s],j,sep="_")
+        
+        if (length(freq1[freq1[, 1] == 0, 2]) > 0) {
+          frequencies[1, 3] <- freq1[freq1[, 1] == 0, 2]
+        }
+        if (length(freq1[freq1[, 1] == 1, 2]) > 0) {
+          frequencies[1, 4] <- freq1[freq1[, 1] == 1, 2]
+        }
+        if (length(freq1[freq1[, 1] == -1, 2]) > 0) {
+          frequencies[1, 5] <- freq1[freq1[, 1] == -1, 2]
+        }
+        if (length(freq1[freq1[, 1] == 2, 2]) > 0) {
+          frequencies[1, 6] <- freq1[freq1[, 1] == 2, 2]
+        }
+        
+        frequencies[is.na(frequencies)] = 0
+        
+        #percent of always suitable areas
+        frequencies[8] <- frequencies[,4] / sum(frequencies[,c(4,5)], na.rm = TRUE) * 100
+        #percent of no longer suitable areas
+        frequencies[9] <- frequencies[,5] / sum(frequencies[,c(4,5)], na.rm = TRUE) * 100
+        #percent of new suitable areas
+        frequencies[10] <- frequencies[,6] / sum(frequencies[,c(4,5)], na.rm = TRUE) * 100
+        #percent of net change 
+        frequencies[11] <- frequencies[,10] - frequencies[,9]
+        #add to changes dataframe
+        changes <- rbind(changes, frequencies)
+        
+        
+      }
+    }
+  }
+  
+  
+  # export dataframe
+  write_csv(changes, paste0("processing/figures/wining_losing_sp_withmask.csv"))
 
+  
+  
+  # Calculate changes ####
+  changes <- read.csv("processing/figures/")
+  changes <- changes[,-c(7:11)]
+  names(changes) <- c("acronym","System","never","remain","no_longer","new")
+  
+  changes %<>% 
+    mutate(area = rep("suitable", nrow(changes)),
+           area_raster = never + remain + no_longer + new, #number of cells (areas) the raster cover
+           cover = ((remain + no_longer) * 100) / area_raster, #proportion of current area
+           future_cover = ((remain + new) * 100) / area_raster, #proportion of future areas
+           change = future_cover  - cover) %>%  #change between current and future suitable areas
+    as_tibble()
+  
+  # make labels of systems (land use)
+  
+  changes$System_label <- as.factor(changes$System)
+  # levels(changes$System_label) <- RCP
+  
+  
+  # add names and labels of focal species 
+  # uses <- sort(unique(changes$main_use))
+  
+  #..............................................
+  #..............................................
+  # Make charts ####
+  # Loop por emission scenario
+  plots <- list()
+  it <- 1
+  for (s in seq_along(scenario)){
+    
+    for(j in c(2:3)){
+      
+      df <- subset(changes, grepl(scenario[s], changes$System))
+      df <- subset(df, grepl(RCP[j], df$System))
+      df$area <- " "
+      head(df)
+      
+      df %<>% arrange(. , change)
+      
+      df$id <- as.integer(factor(df$acronym, levels = unique(df$acronym)))
+      
+      lab <- ""
+      
+      p <- ggplot(data=df) +
+        geom_segment(data=subset(df, df$change <= 0), 
+                     mapping = aes(x=cover, xend=future_cover,
+                                   y=id, yend=id),
+                     # arrow=arrow(length = unit(0.5, "cm")),
+                     size=5, 
+                     color= "#ca0020") +
+        geom_segment(data=subset(df, df$change > 0), 
+                     mapping = aes(x=cover, xend=future_cover,
+                                   y=id, yend=id),
+                     # arrow=arrow(length = unit(0.5, "cm")),
+                     size=5, 
+                     color= "green4") +
+        geom_segment(aes(x=cover-0.5,xend=cover+0.5,y=id,yend=id), size=5, colour="black")+
+        scale_y_continuous(breaks = as.integer(df$id), labels = df$acronym) +
+        scale_x_continuous(breaks = seq(0,100, by=25), limits = c(0,100)) +
+        labs(x = paste0("Suitable area (%)"),
+             y = NULL) +
+        facet_grid(area ~ System_label) +
+        theme(axis.text.y = element_text(size = 10, angle = 0, hjust = 1, 
+                                         vjust = 0.5, face="italic", colour = "black"),
+              axis.text.x = element_text(size = 12, angle = 0, hjust = 0.5, 
+                                         vjust = 1, face="plain", colour = "black"),
+              axis.title = element_text(size = 12, face="bold"),
+              panel.border = element_rect(colour = "black", fill=NA, size=0.5),
+              plot.background = element_blank(),
+              panel.background = element_blank(),
+              strip.text.x = element_text(size = 12, colour = "black"),
+              strip.text.y = element_text(size=12, colour = "black"),
+              strip.background = element_rect(colour="black", fill="#FFFFFF")) 
+      
+      
+      plots[[it]] <- p
+      it <- it+1
+      
+    }
+  }
+  
+  p20 <- grid.arrange(
+    plots[[1]],
+    plots[[2]],
+    nrow = 1)
+  
+  p40 <- grid.arrange(
+    plots[[3]],
+    plots[[4]],
+    nrow = 1)
+  
+  ggsave(paste0("processing/figures/changessuitability_ord_change21-40_withmask.png"),
+         plot = p20,
+         width = 30,
+         height = 15,
+         units = "cm")
+  
+  ggsave(paste0("processing/figures/changessuitability_ord_change41-60_withmask.png"),
+         plot = p40,
+         width = 30,
+         height = 15,
+         units = "cm")
